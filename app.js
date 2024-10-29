00f1080b58524340546cf2931530c150b2358cef
ANarngAung <anarngaung2-17@gmail.com>
2024-10-29 07:08:34 +0630

    added app.js

---
+++ app.js
@@ -0,0 +1,88 @@
+// var message = 'Hello Node!';
+// var sum = 5 + 3;
+// console.log(message);
+// console.log(sum);
+
+// var commandineArgs = process.argv; //process- window / argv- document
+// console.log(commandineArgs);
+
+//node app.js 'Hello Node' From the command line
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const message = "Holle Node!"; //Object
+// message = "Coodbye Node!";
+
+// const sum = 5 + 3;
+// sum += 1;
+
+// console.log(message);
+// console.log(sum);
+
+// const animalArray = ['dog', 'cat', 'pig'] //Array
+// animalArray.push('cow');
+
+// const personObj = {
+//     name: 'chandadhika',
+//     age: 99
+// };
+// personObj.age = 100;
+// personObj.occupation = 'Dseveloper';
+
+// console.log(animalArray);
+// console.log(personObj);
+
+// const printProfileData = printProfileDataArr => {
+//     console.log(printProfileDataArr);
+// };
+// printProfileData(printProfileDataArgs);
+
+// let message = "Hello";
+// message = "Node!";
+// console.log(message);
+
+// let sum = 5 + 3;
+// sum += 1;
+// console.log(sum);
+
+
+// const message = 'Hello Node!';
+
+
+// if (true === true) {
+
+//   const message = 'Hello ES6!';
+
+//   let sum = 5;
+
+//   sum += 10;
+
+//   console.log(message);
+
+//   console.log(sum);
+
+// }
+
+
+// console.log(message);
+
+// console.log(sum);
+
+
+
+// var one = 'one: declared outside the block';
+
+
+// if (true === true) {
+
+//   var one = 'one: declared inside the block'; // notice: we redeclare 'one' here
+
+//   console.log(one); // prints 'one: declared inside the block'
+
+// }
+
+
+// console.log(one);
+
+
